668f339362369b7858f1aa329e94525dc3a5a959
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-03-05 21:22:18 +0200

    Question01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part 01 : Theoretical Questions
+
+            #region Question01
+
+            //Q1: What is an interface in C#?
+            //=>> A contract that a class must follow.
+            //Why do we use interfaces instead of depending on concrete classes directly?
+            //when you need multiple inheritance behavior and loose coupling.
+            //Mention at least three benefits of using interfaces.
+            //loose coupling, easy to extend , easy to test.
+
+            #endregion
+
+            #endregion
         }
     }
 }
